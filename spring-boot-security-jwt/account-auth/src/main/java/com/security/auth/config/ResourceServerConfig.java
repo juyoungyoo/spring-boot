c9576a3cce18c63fed8105fd300d611f4d823f0f
Juyoung <juy5790@outlook.com>
2019-05-28 18:08:21 +0900

    update

--- spring-boot-security-jwt/account-auth/src/main/java/com/security/auth/config/ResourceServerConfig.java
+++ spring-boot-security-jwt/account-auth/src/main/java/com/security/auth/config/ResourceServerConfig.java
@@ -4,26 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 
 @Configuration
-@EnableResourceServer
+//@EnableResourceServer
 @Profile("mvc")
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
         http
                 .authorizeRequests()
-                .antMatchers("/employee").hasRole("ADMIN")
+                .antMatchers("/login", "/sign-in", "/sign-up").permitAll()
+                .antMatchers("/oauth/token/revokeById/**").permitAll()
+                .antMatchers("/tokens/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
+                .formLogin()
+                .and()
                 .csrf().disable()
+                .httpBasic()
         ;
     }
 
--- spring-boot-security-jwt/account-auth/src/main/java/com/security/auth/config/SecurityConfig.java
+++ spring-boot-security-jwt/account-auth/src/main/java/com/security/auth/config/SecurityConfig.java
@@ -12,11 +12,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 
 
 @Configuration
+@EnableResourceServer
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -44,17 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/h2-console/**");
     }
 
+
     @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        // @formatter:off
-//        http.authorizeRequests().antMatchers("/login").permitAll()
-//                .antMatchers("/oauth/token/revokeById/**").permitAll()
-//                .antMatchers("/tokens/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and().formLogin().permitAll()
-//                .and().csrf().disable();
-        // @formatter:on
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+    public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/login", "/sign-in", "/sign-up").permitAll()
@@ -70,5 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         ;
     }
 
+    @Autowired
+    DefaultTokenServices defaultTokenServices;
 
 }
