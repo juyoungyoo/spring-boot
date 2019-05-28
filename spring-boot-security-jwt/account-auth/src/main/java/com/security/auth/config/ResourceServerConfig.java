e0a83afe92e52911727e98c0bfb94e0da6c7894f
Juyoung <juy5790@outlook.com>
2019-05-28 19:27:38 +0900

    implicit 동작 - jwt token 작동안함

--- spring-boot-security-jwt/account-auth/src/main/java/com/security/auth/config/ResourceServerConfig.java
+++ spring-boot-security-jwt/account-auth/src/main/java/com/security/auth/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.security.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -10,8 +11,16 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 
 @Configuration
 @EnableResourceServer
-//@Profile("mvc")
+@Profile("mvc")
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    DefaultTokenServices defaultTokenServices;
+
+    @Override
+    public void configure(final ResourceServerSecurityConfigurer config) {
+        config.tokenServices(defaultTokenServices);
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -28,13 +37,5 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .csrf().disable()
                 .httpBasic()
         ;
-    }
-
-    @Autowired
-    DefaultTokenServices defaultTokenServices;
-
-    @Override
-    public void configure(final ResourceServerSecurityConfigurer config) {
-        config.tokenServices(defaultTokenServices);
     }
 }
--- spring-boot-security-jwt/account-auth/src/main/java/com/security/auth/config/SecurityConfig.java
+++ spring-boot-security-jwt/account-auth/src/main/java/com/security/auth/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -40,6 +41,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) {
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
         web.ignoring().antMatchers("/h2-console/**");
+    }
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/login", "/sign-in", "/sign-up").permitAll()
+                .antMatchers("/oauth/token/revokeById/**").permitAll()
+                .antMatchers("/tokens/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .csrf().disable()
+                .httpBasic()
+        ;
     }
 
 }
