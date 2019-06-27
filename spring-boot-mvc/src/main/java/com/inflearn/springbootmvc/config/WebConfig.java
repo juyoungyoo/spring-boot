adbd36b553611c8a3b0f085f8fa7f6992d07b1a1
Juyoung <juy5790@outlook.com>
2019-06-27 14:37:50 +0900

    spring mvc : static resource

---
+++ spring-boot-mvc/src/main/java/com/inflearn/springbootmvc/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.inflearn.springbootmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/")  // 반드시 /로 끝나야한다
+                .setCachePeriod(20);
+
+    }
+}
